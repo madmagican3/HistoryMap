3aa81fdf3f8bbf9d9f1614d2ef542cd6607257c4
andrew beaumont <madmagican3@hotmail.com>
2017-07-19 07:07:37 +0100

    Commented the code

--- HistoryMap/Shared Classes/LocalSQLGetter.cs
+++ HistoryMap/Shared Classes/LocalSQLGetter.cs
@@ -14,31 +14,53 @@ namespace HistoryMap.Shared_Classes
     {
         public static Dictionary<Color, List<Point>> getCountries(DateTime currentTime)
         {
-            string connectionString = "Data Source=192.168.1.83;Initial Catalog=History_Map;Integrated Security=SSPI;";
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(null, connection))
-                {
-                    command.CommandText = "Select id from IMAGES where date = @date";
-                    SqlParameter dateParam = new SqlParameter("@date",SqlDbType.Date);
-                    dateParam.Value = currentTime;
-
-                    command.Prepare();
-
-                    DataSet actualId = SelectRows(new DataSet(), command);
-                }
-            }
             return null;
         }
-
-        private static DataSet SelectRows(DataSet dataset, SqlCommand command)
+        /// <summary>
+        /// This takes the dataset and then selects the rows based on the command and passes the databset back
+        /// </summary>
+        /// <param name="command">This is the command run</param>
+        /// <returns></returns>
+        private static DataSet SelectRows(SqlCommand command)
         {
-
-                SqlDataAdapter adapter = new SqlDataAdapter();
-                adapter.SelectCommand = command;
-                adapter.Fill(dataset);
-                return dataset;
+            //creates a new empty dataset and adapter
+            var dataset = new DataSet();
+            var adapter = new SqlDataAdapter();
+            //set the adapter to use the command 
+            adapter.SelectCommand = command;
+            //then get the adapter to fill the dataset based on the returned values
+            adapter.Fill(dataset);
+            return dataset;
+        }
+        /// <summary>
+        /// This gets the date id based on the current date
+        /// </summary>
+        /// <param name="currentTime">This is the current date</param>
+        /// <returns></returns>
+        private DataSet GetDateId(DateTime currentTime)
+        {
+            //This is the connection string, will probably be re-written soon for security purposes but unsure how to handle that currently (also no ports open so currently secure)
+            var connectionString = "Data Source=192.168.1.83;Initial Catalog=History_Map;Integrated Security=SSPI;";
+            //create a new connection
+            using (var connection = new SqlConnection(connectionString))
+            {
+                //open that connection
+                connection.Open();
+                //create a new command
+                using (var command = new SqlCommand(null, connection))
+                {
+                    //set that new command using a prepared statement
+                    command.CommandText = "Select id from IMAGES where date = @date";
+                    //safely put that paramater into the sql statement
+                    var dateParam = new SqlParameter("@date", SqlDbType.Date);
+                    dateParam.Value = currentTime;
+                    //prepare the new command
+                    command.Prepare();
+                    //get the dataset back and return it
+                    var actualId = SelectRows( command);
+                    return actualId;
+                }
+            }
         }
     }
 }
