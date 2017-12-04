26182a340bd4ac7345594cec51a4d8356a05052f
andrew beaumont <madmagican3@gmail.com>
2017-12-04 14:13:43 +0000

    created the form for world map contribution but havent done much with it yet

---
+++ HistoryMap/WorldMapUsers/WorldMapContributor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HistoryMap.WorldMapUsers
+{
+    public partial class WorldMapContributor : Form
+    {
+        public WorldMapContributor()
+        {
+            InitializeComponent();
+        }
+
+        private void WorldMapContributor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WorldMapContributor_ResizeEnd(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
